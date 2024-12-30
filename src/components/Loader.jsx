142154995ead5dbff533566c120067c1d0824573
Sachin2310 <warlord2op@gmail.com>
2024-12-30 13:47:10 +0530

    feat: added loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import { CircularProgress } from "@mui/material";
+
+const Loader = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+      }}
+    >
+      <CircularProgress />
+    </div>
+  );
+};
+
+export default Loader;
